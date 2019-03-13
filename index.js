ef9c81fab211d2b9df4828d4236ce137cfb428bc
camilo orrante <camilo.orrante@gmail.com>
2019-03-13 02:45:00 +0000

    se cambio el puerto perro

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("jatasss");
+var http = require("http");
+
+http.createServer(function(request, response){
+	response.writeHead(200);
+}).listen(8082);
+
+console.log("app listening on 8082");
